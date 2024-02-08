3cf19e2929e324468333bb89aef261d6b93722f0
IamEzio <anshumanmaurya111@gmail.com>
2024-02-08 14:06:13 +0530

    added mal url csv file

--- api/utils.py
+++ api/utils.py
@@ -1,10 +1,11 @@
 import json
 import requests
 import urllib.parse
+import pandas as pd
 
 ipQualityKey = "bWZjrc5gAVlpmPq20zVkWVnSCsmF3dvz"
 keysToCheck = ["spamming", "malware", "phishing", "suspicious", "adult"]
-
+malicious_url_csv_file = 'api/malicious_urls.csv'
 
 def malicious_URL_scan(url, vars={}):
     url = "https://www.ipqualityscore.com/api/json/url/%s/%s" % (
@@ -21,3 +22,10 @@ def malicious_URL_scan(url, vars={}):
         "risk_score": data["risk_score"],
         "categories": presentCategories,
     }
+
+
+def check_urls_in_csv(urls_to_check):
+    df = pd.read_csv(malicious_url_csv_file)
+    url_column = df['url']
+    result_list = [1 if url in url_column.values else 0 for url in urls_to_check]
+    return result_list
