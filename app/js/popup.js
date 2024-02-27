4d1ffca6e363a14c7bf8a4c30b22c65ecb18de38
shivansh-bhatnagar18 <shivansh111sid@gmail.com>
2024-02-27 10:47:58 +0530

    Changes in the bar graph for dps

--- app/js/popup.js
+++ app/js/popup.js
@@ -21,7 +21,29 @@ chrome.runtime.onMessage.addListener(function (request, _sender, _sendResponse) 
 	console.log(`[RECIEVED] [${request.message}]`);
 	switch (request.message) {
 		case "update_current_count":
-			document.getElementsByClassName("number")[0].textContent = request.count;
+			const countArray = [];
+			for (const key in request.count) {
+			const value = request.count[key];
+			countArray.push([key, value]);
+			}
+			console.log(countArray);
+			document.getElementsByClassName("number")[0].textContent = countArray[0][1];
+			const dp1Element = document.getElementsByClassName("dp1")[0];
+			dp1Element.getElementsByTagName("em")[0].textContent = countArray[1][0];
+			dp1Element.getElementsByTagName("span")[0].textContent = countArray[1][1];
+			const dp2Element = document.getElementsByClassName("dp2")[0];
+			dp2Element.getElementsByTagName("em")[0].textContent = countArray[2][0];
+			dp2Element.getElementsByTagName("span")[0].textContent = countArray[2][1];
+			const dp3Element = document.getElementsByClassName("dp3")[0];
+			dp3Element.getElementsByTagName("em")[0].textContent = countArray[3][0];
+			dp3Element.getElementsByTagName("span")[0].textContent = countArray[3][1];
+			const dp4Element = document.getElementsByClassName("dp4")[0];
+			dp4Element.getElementsByTagName("em")[0].textContent = countArray[4][0];
+			dp4Element.getElementsByTagName("span")[0].textContent = countArray[4][1];
+			const dp5Element = document.getElementsByClassName("dp5")[0];
+			dp5Element.getElementsByTagName("em")[0].textContent = countArray[5][0];
+			dp5Element.getElementsByTagName("span")[0].textContent = countArray[5][1];
+			setBarWidth(".style-1 span", ".style-1 em", "padding-right", 50);
 			break;
 
 		case "start_loading_screen":
@@ -31,8 +53,22 @@ chrome.runtime.onMessage.addListener(function (request, _sender, _sendResponse) 
 		case "stop_loading_screen":
 			document.getElementById("loader").style.display = "none";
 			break;
+			
 		case "update_selection":
 			localStorage.setItem("darkBustSelectedContent", request.content);
 			break;
 	}
+
+	function setBarWidth(dataElement, barElement, cssProperty, barPercent) {
+		var listData = Array.from(document.querySelectorAll(dataElement)).map(function(element) {
+			return element.innerHTML;
+		});
+		var listMax = Math.max(...listData);
+		Array.from(document.querySelectorAll(barElement)).forEach(function(element, index) {
+			element.style[cssProperty] = (listData[index] / listMax) * barPercent + "%";
+		});
+	}
 });
+
+
+
